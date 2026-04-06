e23f63fa8c265287d0830ab1f7db5454ffab1051
Lakshya Gupta <mailto.lakshyagupta25@gmail.com>
2026-04-06 17:17:27 +0530

    used fseek and fgetc to stop loading into memory

--- q5/q5.s
+++ q5/q5.s
@@ -1,6 +1,6 @@
 .section .data
     filename: .string "input.txt"
-    buffer: .space 1024
+    mode: .string "r"
     outputyes: .string "Yes\n"
     outputno: .string "No\n"
 
@@ -8,45 +8,70 @@
 .globl main
 
 main:
-    addi sp, sp, -16 # store RA in stack
+    addi sp, sp, -32 # store RA in stack
+    sd s0, 24(sp)
+    sd s1, 16(sp)
     sd ra, 8(sp)
+    sd s2, 0(sp)
 
     la a0, filename
-    li a1, 0
-    call open # first argument is filename, mode (read only => 0)
-    
-    mv s0, a0
+    la a1, mode 
+    call fopen # fopen("input.txt", "r") 
+    mv s0, a0 # gives file pointer
 
-    mv a0, s0 # puting all the text into buffer
-    la a1, buffer
-    li a2, 1024
-    call read
-       
+    mv a0, s0
+    li a1, 0
+    li a2, 2 # SEEK_END = 2
+    call fseek # fseek(stream, 0, SEEK_END)
+    
+    mv a0, s0
+    call ftell # ftell(stream)
+
     mv s1, a0           # length of string
 
-    la t0, buffer      
-    add t1, t0, s1      # Address after the last char
-    addi t1, t1, -1     # End pointer (right)
+    addi s2, x0, 0# left
+    addi s3, s1, -1  #right
 
 loop:
-    bge t0, t1, yes # left > right its a palindrome
-    lbu t2, 0(t0)       # left
-    lbu t3, 0(t1)       # right 
-    bne t2, t3, no # If not equal then not a palindrome
-    addi t0, t0, 1
-    addi t1, t1, -1
+    bge s2, s3, yes # left >= right its a palindrome
+
+    mv a0, s0
+    mv a1, s2
+    li a2, 0
+    call fseek # first argument is fileptr and second is the location
+    mv a0, s0
+    call fgetc
+    mv t0, a0 # left char
+
+    mv a0, s0
+    mv a1, s3
+    li a2, 0 # SEEK SET
+    call fseek # first argument is fileptr and second is the location
+    mv a0, s0
+    call fgetc
+    mv t1, a0 # right char
+
+    bne t0, t1, no # If not equal then not a palindrome
+    addi s2, s2, 1
+    addi s3, s3, -1
     j loop
 
 yes:
     la a0, outputyes
     call printf # print Yes
+    ld s0, 24(sp)
+    ld s1, 16(sp)
     ld ra, 8(sp)  
-    addi sp, sp, 16
+    ld s2, 0(sp)
+    addi sp, sp, 32
     ret
 
 no:
     la a0, outputno
     call printf # print No
+    ld s0, 24(sp)
+    ld s1, 16(sp)
     ld ra, 8(sp)  
-    addi sp, sp, 16
+    ld s2, 0(sp)
+    addi sp, sp, 32
     ret
